5456b7db11190ba96b82273b7f9d98557716b51c
Mihai Nechita <mihai.nechita95@gmail.com>
2019-01-24 22:22:20 +0200

    initial signup form

--- AGRe/urls.py
+++ AGRe/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^core/', include('core.urls')),
+
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home')
+
 ]
