5c3a3780d34ae8c2fd94fe8806d4988a0f775801
Andrei Iacob <andreiacob37@gmail.com>
2019-01-27 19:25:13 +0200

    Update structure for courses

--- scripts/add_mooc_courses2rdf.py
+++ scripts/add_mooc_courses2rdf.py
@@ -27,6 +27,7 @@ s_person = URIRef('http://schema.org/Person')
 s_action = URIRef('http://schema.org/Action')
 s_creative_work = URIRef('http://schema.org/CreativeWork')
 s_organization = URIRef('http://schema.org/Organization')
+s_edu_org = URIRef('http://schema.org/EducationalOrganization')
 s_book = URIRef('http://schema.org/Book')
 s_book_format_type = URIRef('http://schema.org/BookFormatType')
 s_course = URIRef('http://schema.org/Course')
@@ -51,22 +52,22 @@ def add_course(g, course):
         g.add((uri, name, Literal(course['title'])))
 
     if 'instructors' in course:
-        author = URIRef('http://schema.org/instructor')
+        s_instructor = URIRef('http://schema.org/instructor')
         for instructor in course['instructors']:
-            g.add((uri, author, URIRef(instructor['url'])))
+            g.add((uri, s_instructor, URIRef(instructor['url'])))
             
     if 'language' in course:
         inLanguage = URIRef('http://schema.org/inLanguage')
         g.add((uri, inLanguage, Literal(course['language']['text'])))
 
     if 'provider' in course:
-        provider = URIRef('http://schema.org/provider')
-        g.add((uri, provider, URIRef(course['provider']['url'])))
+        s_offeredBy = URIRef('http://schema.org/offeredBy')
+        g.add((uri, s_offeredBy, URIRef(course['provider']['url'])))
 
     if 'universities' in course:
-        s_location = URIRef('http://schema.org/location')
+        s_provider = URIRef('http://schema.org/provider')
         for university in course['universities']:
-            g.add((uri, s_location, URIRef(university['url'])))
+            g.add((uri, s_provider, URIRef(university['url'])))
 
     if 'country' in course:
         location = URIRef('http://schema.org/location')
@@ -113,19 +114,47 @@ def add_generic(g, l, type=None):
         if 'name' in thing:
             g.add((uri, name, Literal(thing['name'])))
 
+def add_category_names(g, courses):
+    categories = set()
+    name = URIRef('http://schema.org/name')
+
+    for course in tqdm.tqdm(courses):
+        if 'categories' in course:
+            for category in course['categories']:
+                category_url = category['url']
+                if category_url not in categories:
+                    categories.add(category_url)
+                    g.add((URIRef(category_url), name, Literal(category['text'])))
+
 def add_enteties_from_courses(g, courses):
     instructors = dict()
     universities = set()
     providers = set()
+    countries = set()
     worksFor = URIRef('http://schema.org/worksFor')
+    s_country = URIRef('http://schema.org/Country')
+    name = URIRef('http://schema.org/name')
 
     for course in tqdm.tqdm(courses):
+        if 'categories' in course:
+            for category in course['categories']:
+                g.add((URIRef(category['url']), name, Literal(category['text'])))
+
+        if 'country' in course:
+            country = course['country']['url']
+            country_ref = URIRef(country)
+            country_lit = Literal(course['country']['text'])
+            if country not in countries:
+                countries.add(country)
+                g.add((country_ref, RDF.type, s_country))
+                g.add((country_ref, name, country_lit))
+
         if 'universities' in course:
             for uni in course['universities']:
                 uni_url = uni['url']
                 uni_ref = URIRef(uni_url)
                 if uni_url not in universities:
-                    g.add((uni_ref, RDF.type, s_organization))
+                    g.add((uni_ref, RDF.type, s_edu_org))
                     universities.add(uni_url)
 
         if 'provider' in course:
@@ -158,14 +187,16 @@ if __name__ == "__main__":
     with open('data/mooc_list_courses_detailed.json', 'r') as f:
         courses = json.load(f)
 
-    courses = courses[:100]
+    courses = courses
 
     add_enteties_from_courses(update_g, courses)
     for course in tqdm.tqdm(courses):
         add_course(update_g, course)
 
-    files = ['data/mooc_list_instructors.json', 'data/mooc_list_universities.json', 'data/mooc_list_providers.json']
+    add_category_names(update_g, courses)
+
+    files = ['data/mooc_list_instructors.json', 'data/mooc_list_universities.json', 'data/mooc_list_providers.json', 'data/mooc_list_courses_detailed.json']
     for file in files:
         with open(file, 'r') as f:
             l = json.load(f)
-        add_generic(update_g, l[:100])
+        add_generic(update_g, l)
